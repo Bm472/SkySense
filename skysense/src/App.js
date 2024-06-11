a99f48feb75d04dabdd240da6dc5da7a54b4ceb2
Bradley <94051876+Bm472@users.noreply.github.com>
2024-06-11 01:11:22 +0100

    Implemented API calls to retrieve location and weather
    Made api calls to retrieve client location and weather in local area. Displayed this data in a very simple way.

--- skysense/src/App.js
+++ skysense/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [city,setCity] = useState("");
+  const [weather,setWeather] = useState([]);
+
+  useEffect(() => getLocation(),[city]);
+
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {city}
+        <div>Cloud Cover: {weather.cloudcover}</div>
+        <div>Conditions: {weather.conditions}</div>
+        <div>Date/Time: {weather.datetime}</div>
+        <div>Dew: {weather.dew}</div>
+        <div>Feels Like: {weather.feelslike}C</div>
+        <div>Humidity: {weather.humidity}</div>
+        <div>Icon: {weather.icon}</div>
+        <div>Moon Phase: {weather.moonphase}</div>
+        <div>Precipitation: {weather.precip}</div>
+        <div>Precipitation Chance: {weather.precipprob}%</div>
+        <div>Precipitation Type: {weather.preciptype}</div>
+        <div>Pressure: {weather.pressure}</div>
+        <div>Snow: {weather.snow}</div>
+        <div>Snow Depth: {weather.snowdepth}</div>
+        <div>Sunrise: {weather.sunrise}</div>
+        <div>Sunset: {weather.sunset}</div>
+        <div>Temperature: {weather.temp}</div>
+        <div>UV Index: {weather.uvindex}</div>
+        <div>Visibility: {weather.visibility}</div>
+        <div>Wind Direction: {weather.winddir}</div>
+        <div>Wind Gust: {weather.windgust}</div>
+        <div>Wind Speed: {weather.windspeed}</div>
       </header>
-    </div>
+    </>
   );
+
+
+
+
+
+function getLocation() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+}
+
+function showPosition(position) {
+  fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`).then((res) => res.json()).then((data) =>
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${data.city}?unitGroup=metric&key=GBEPU93ZW6U272C5NLSD6QLBM&contentType=json`).then((data) => data.json()).then((data) => {
+    setCity(data.address);
+    setWeather(data.currentConditions);
+    console.log(weather);
+    }
+));
+  
+}
+
+
+
+
+
 }
 
 export default App;
